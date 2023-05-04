59bb9be32e8c6ab552af6da257890b47e964b4e1
AstonMartinLim <113468656+AstonMartinLim@users.noreply.github.com>
2023-05-04 21:37:38 +0300

    add files

---
+++ shop/main_app/exceptions.py
@@ -0,0 +1,16 @@
+class NotEnoughMoneyException(BaseException):
+    """
+    Not enough money
+    """
+
+
+class NotEnoughProductException(BaseException):
+    """
+    Not enough products
+    """
+
+
+class ReturnTimeExpired(BaseException):
+    """
+    Time is over
+    """
